99bc2026b09a536398d2a7253d757e6f88ee795a
Elozino <ovedhee@gmail.com>
2023-08-29 15:56:24 +0100

    Changed the spelling of "COntextWrapper" to "ContextWrapper"

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import ContextWrapper from './context/COntextWrapper.jsx'
+import ContextWrapper from './context/ContextWrapper.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
